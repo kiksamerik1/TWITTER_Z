5a0fc5492bf5181ae0d4533c274b75189829bb2d
Karim Lahouegue <lahouegue.karim@gmail.com>
2012-04-09 23:16:18 +0200

    Initial Commit

---
+++ app/controllers/zombies_controller.rb
@@ -0,0 +1,40 @@
+class ZombiesController < ApplicationController
+	
+	def index
+		@zombies = Zombie.all
+		respond_to do |format|
+			format.html
+			format.xml { render :xml => @zombies}
+			format.json { render :json => @zombies}  
+		end	
+	end
+
+	def show
+		
+	end
+
+	def new
+		@zombie = Zombie.new
+	end
+
+	def edit
+		@zombie = Zombie.find(params[:id])
+	end
+
+	def create
+		Zombie.create(params[:zombie])
+		redirect_to zombies_path	
+	end
+
+	def update
+		@zombie=Zombie.find(params[:id])
+		@zombie.update_attributes(params[:zombie])
+		redirect_to zombies_path
+	end
+
+	def destroy
+		@zombie = Zombie.find(params[:id])
+		@zombie.destroy
+		redirect_to zombies_path
+	end	
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TWITTERZ::Application.config.secret_token = '72608dffb2fc26ba34a80aea12735e3951e82d0cc344b6f8b321b6ad9a08684dc6e426f033fe7e4328cced4c278c8a7decf48269d9a2551cfdb90b83a8e43d6e'
