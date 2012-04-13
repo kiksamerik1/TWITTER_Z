50d34d0384d3126bc351a7058c61d29b791e2546
Karim Lahouegue <lahouegue.karim@gmail.com>
2012-04-13 23:34:55 +0200

    14042012

--- app/controllers/zombies_controller.rb
+++ app/controllers/zombies_controller.rb
@@ -1,5 +1,11 @@
 class ZombiesController < ApplicationController
+
+	before_filter :find_zombie, :only => [:show, :edit, :update, :destroy]
 	
+	def find_zombie
+		@zombie = Zombie.find params[:id]
+	end
+
 	def index
 		@zombies = Zombie.all
 		respond_to do |format|
@@ -7,34 +13,38 @@ class ZombiesController < ApplicationController
 			format.xml { render :xml => @zombies}
 			format.json { render :json => @zombies}  
 		end	
+	end	
+	
+	def show
 	end
 
-	def show
-		@zombie = Zombie.find params[:id]
+	def edit
 	end
 
 	def new
 		@zombie = Zombie.new
 	end
 
-	def edit
-		@zombie = Zombie.find(params[:id])
-	end
-
 	def create
-		Zombie.create(params[:zombie])
-		redirect_to zombies_path	
+		@zombie = Zombie.new(params[:zombie])
+		if @zombie.save
+			flash[:notice] = "#{@zombie.name} was successfully added!"	
+			redirect_to zombies_path
+		else
+			render :action=>'new'
+		end		
 	end
 
 	def update
-		@zombie=Zombie.find(params[:id])
-		@zombie.update_attributes(params[:zombie])
-		redirect_to zombies_path
+		@zombie.update_attributes!(params[:zombie])
+		flash[:notice] = "#{@zombie.name} was successfully updated!"
+		redirect_to zombie_path(@zombie)
 	end
 
 	def destroy
-		@zombie = Zombie.find(params[:id])
 		@zombie.destroy
+		flash[:notice] = "Zombie '#{@zombie.name}' deleted."
 		redirect_to zombies_path
-	end	
+	end
+
 end
--- app/models/zombie.rb
+++ app/models/zombie.rb
@@ -1,2 +1,4 @@
 class Zombie < ActiveRecord::Base
+	validates :name, :presence=>true
+	validates :graveyard, :presence=>true
 end 
